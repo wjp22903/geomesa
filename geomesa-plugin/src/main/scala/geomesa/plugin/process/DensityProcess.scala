bb9fc8cf0b7f2a83c12c99869b9a3cb24c05ace5
Anthony Fox <anthony@ccri.com>
2014-04-03 16:00:08 -0400

    Propagate options

--- geomesa-plugin/src/main/scala/geomesa/plugin/process/DensityProcess.scala
+++ geomesa-plugin/src/main/scala/geomesa/plugin/process/DensityProcess.scala
@@ -1,7 +1,7 @@
 package geomesa.plugin.process
 
 import org.geotools.process.vector.HeatmapProcess
-import org.geotools.process.factory.DescribeProcess
+import org.geotools.process.factory.{DescribeParameter, DescribeProcess}
 import org.geotools.geometry.jts.ReferencedEnvelope
 import org.geotools.data.Query
 import org.opengis.coverage.grid.GridGeometry
@@ -12,10 +12,10 @@ import geomesa.core.data.AccumuloFeatureReader
   description = "Computes a heatmap surface over a set of data points and outputs as a single-band raster."
 )
 class DensityProcess extends HeatmapProcess {
-  override def invertQuery(argRadiusPixels: Integer,
-                           argOutputEnv: ReferencedEnvelope,
-                           argOutputWidth: Integer,
-                           argOutputHeight: Integer,
+  override def invertQuery(@DescribeParameter(name = "radiusPixels", description = "Radius to use for the kernel", min = 0, max = 1) argRadiusPixels: Integer,
+                           @DescribeParameter(name = "outputBBOX", description = "Georeferenced bounding box of the output") argOutputEnv: ReferencedEnvelope,
+                           @DescribeParameter(name = "outputWidth", description = "Width of the output raster")  argOutputWidth: Integer,
+                           @DescribeParameter(name = "outputHeight", description = "Height of the output raster") argOutputHeight: Integer,
                            targetQuery: Query,
                            targetGridGeometry: GridGeometry): Query = {
     val q =
