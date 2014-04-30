778ce2dfe0f1be6550af506db8132633b9c50f56
Jim Hughes <jnh5y@ccri.com>
2014-04-30 16:19:47 -0400

    Set up MiniAccumulo in a new test which can be run with
    > mvn -Dtest=MiniMesaTest test -pl geomesa-core

---
+++ geomesa-core/src/test/scala/geomesa/core/minimesa/MiniMesaTest.scala
@@ -0,0 +1,62 @@
+package geomesa.core.minimesa
+
+import java.io.File
+import org.apache.accumulo._
+import org.apache.accumulo.core._
+import org.apache.accumulo.minicluster._
+import org.apache.accumulo.start._
+import org.junit._
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import org.specs2.mutable.Specification
+import geomesa.utils.geotools.GeneralShapefileIngest
+import java.util.{Map => JMap}
+import scala.collection.JavaConversions._
+import org.geotools.data.DataStoreFinder
+
+
+@RunWith(classOf[JUnitRunner])
+class MiniMesaTest extends Specification {
+
+  //
+  //  val folder = new TemporaryFolder(new File("/tmp/"))
+
+  "MiniAccumulo" should {
+
+    "Not be null" in {
+      val tmp = new File(s"/tmp/${scala.util.Random.alphanumeric.take(5).mkString}")
+
+      System.out.println(s"Tmp  dir: $tmp")
+
+      val passwd = "secret"
+      val ma = new MiniAccumuloCluster(tmp, passwd)
+
+      ma.start()
+
+      System.out.println("Started MiniAccumulo")
+
+      val zk: String = ma.getZooKeepers
+
+      val villages = "/opt/devel/src/SPINOZA-RPMS/spinoza-geo-data/target/af/spinoza_map_data/features/af_villages_u.shp"
+
+      val maparams = Map("instanceId" -> "miniInstance", "zookeepers" -> zk, "user" -> "root",
+        "password" -> new String(passwd), "auths" -> "", "tableName" -> "matest")
+
+      System.out.println("Ingesting Shapefile")
+
+      GeneralShapefileIngest.shpToDataStoreViaParams(villages, maparams)
+
+      System.out.println("Ingested shapefile")
+
+      val ds = DataStoreFinder.getDataStore(maparams)
+      val fs = ds.getFeatureSource(ds.getNames.head)
+      val size = fs.getFeatures().size
+
+      System.out.println(s" Size of collection is $size.")
+
+      ma.stop()
+
+    }
+
+  }
+}
