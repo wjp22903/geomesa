d7dd72c800aa2622054efc45aef8a0fe25280a6e
rattai <rattai@ccri.com>
2014-05-26 14:19:00 -0400

    STEALTH-6 Downgrade Jetty from 9 to 8
    Also, increased cross-origin proxy timeout from 30 seconds to 1 hour.
    
    Reviewed by @rattai

--- src/main/java/com/ccri/stealth/web/CorsProxyServlet.java
+++ src/main/java/com/ccri/stealth/web/CorsProxyServlet.java
@@ -1,17 +1,34 @@
 package com.ccri.stealth.web;
 
-import org.eclipse.jetty.proxy.ProxyServlet;
+//Jetty 8
+import org.eclipse.jetty.http.HttpURI;
+import org.eclipse.jetty.servlets.ProxyServlet;
+
+//Jetty 9
+//import org.eclipse.jetty.proxy.ProxyServlet;
 
 import javax.servlet.http.HttpServletRequest;
+import java.net.MalformedURLException;
 import java.net.URI;
 
 public class CorsProxyServlet extends ProxyServlet {
+    //Jetty 8
     @Override
+    protected HttpURI proxyHttpURI(HttpServletRequest request, String uri) throws MalformedURLException {
+        String query = request.getQueryString();
+        return new HttpURI(URI.create((
+                request.getPathInfo() +
+                        ((query != null && !query.isEmpty()) ? ("?" + query) : "")
+        ).substring(1)));
+    }
+
+    //Jetty 9
+    /*@Override
     protected URI rewriteURI(HttpServletRequest request) {
         String query = request.getQueryString();
         return URI.create((
             request.getPathInfo() +
             ((query != null && !query.isEmpty()) ? ("?" + query) : "")
         ).substring(1));
-    }
+    }*/
 }
