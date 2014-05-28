9f787a60bd385be10c2fe205272409773e5b68fe
mrk239 <mkrofcheck@ccri.com>
2014-05-28 15:12:23 -0400

    STEALTH-7 Select a geoserver, select a layer...
    - Modified add sites form to allow user to select a geoserver url,
      select a layer, view info for that layer, and enter a cql query.
    
    - Added WFS factory to ows module.
    
    - Styled the form a bit.
    
    Reviewed by @rattai

--- src/main/ui/src/app/ows/ows.js
+++ src/main/ui/src/app/ows/ows.js
@@ -8,24 +8,37 @@ angular.module('stealth.ows.ows', [])
         }
     })
 
-    .config(function () {
-        // TODO - this should be set in the application config as a constant
-        // and injected into this module.
-        OpenLayers.ProxyHost = "cors/";
-    })
+    .config(['CONFIG', function (CONFIG) {
+        OpenLayers.ProxyHost = CONFIG.geoserver.proxyHost;
+    }])
 
-    .factory('WMS', ['$q', '$http', 'CONFIG', function ($q, $http, CONFIG) {
+    // Formats the url to have the specified endpoint.
+    .filter('endpoint', function () {
+        return function (url, pathname, omitProxy) {
+            // Get the geoserver root with no trailing slash.
+            var uri = url.replace(/(\/|wms|wfs)+$/, "");
+            uri += '/' + pathname;
+            if (!omitProxy) {
+                uri = OpenLayers.ProxyHost + uri;
+            }
+            return uri;
+        };
+    })
+    
+    // WMS Utilities
+    .factory('WMS', ['$q', '$http', '$filter', 'CONFIG', function ($q, $http, $filter, CONFIG) {
         
         var parser = new OpenLayers.Format.WMSCapabilities(),
-            capabilities;
+            capabilities = {};
         
         // Requests the wms capabilities from geoserver and returns a promise.
-        function requestCapabilities () {
+        function requestCapabilities (url) {
+
             var deferred = $q.defer();
             
             // Using open layers rather than $http because of the ProxyHost.
             OpenLayers.Request.GET({
-                url: CONFIG.geoserver.url + 'wms',
+                url: url,
                 params: {
                     SERVICE: 'WMS',
                     REQUEST: 'GetCapabilities'
@@ -34,6 +47,10 @@ angular.module('stealth.ows.ows', [])
                     if(response && response.responseText) {
                         deferred.resolve(parser.read(response.responseText));
                     }
+                },
+                failure: function (response) {
+                    // TODO - block other fields here
+                    deferred.reject(response);
                 }
             });
 
@@ -42,15 +59,100 @@ angular.module('stealth.ows.ows', [])
 
         return {
             // Requests capabilities (or returns the cached version) as a promise.
-            getCapabilities: function () {
-                if(angular.isDefined(capabilities)) {
-                    return $q.when(capabilities);
+            getCapabilities: function (url) {
+
+                // TODO url validation.
+                var uri = $filter('endpoint')(url, 'wms');
+                
+                if(angular.isDefined(capabilities[uri])) {
+                    return $q.when(capabilities[uri]);
                 }
 
-                return requestCapabilities().then(function (data) {
-                    capabilities = data;
-                    return capabilities;
+                return requestCapabilities(uri).then(function (data) {
+                    capabilities[uri] = data;
+                    return capabilities[uri];
                 });
             }
         };
+
+    }])
+    
+    // WFS Utilities.
+    .factory('WFS', ['$q', '$http', '$filter', 'CONFIG', function ($q, $http, $filter, CONFIG) {
+
+        var parser = new OpenLayers.Format.WFSDescribeFeatureType(),
+            descriptions = {};
+
+        // Makes a DescribeFeatureType request to GeoServer and returns a promise.
+        function requestFeatureTypeDescription (url, typeName) {
+            var deferred = $q.defer();
+            
+            // Using open layers rather than $http because of the ProxyHost.
+            OpenLayers.Request.GET({
+                url: url,
+                params: {
+                    SERVICE: 'WFS',
+                    REQUEST: 'describeFeatureType',
+                    TYPENAME: typeName
+                },
+                success: function (response) {
+                    if(response && response.responseText) {
+                        deferred.resolve(parser.read(response.responseText));
+                    }
+                },
+                failure: function (response) {
+                    // block other fields here
+                            
+                    deferred.reject(response);
+                }
+            });
+
+            return deferred.promise;
+        }
+
+        function getFeature (url, typeName, paramOverrides) {
+            // TODO - validate arguments.
+            var uri = url,
+                paramDefaults = {
+                    service: 'WFS',
+                    version: '2.0.0',
+                    request: 'GetFeature',
+                    typeName: typeName,
+                    outputFormat: 'application/json',
+                };
+                
+            return $http.get(uri, { params:  _.merge(paramDefaults, paramOverrides) });
+        }
+
+        return {
+            // Requests a description for the feature type (or returns cached)
+            // as a promise. (DescribeFeatureType)
+            getFeatureTypeDescription: function (url, typeName) {
+                // TODO - validate arguments.
+                var uri = $filter('endpoint')(url, 'wfs');
+
+                // Return cached if it exists.
+                if (angular.isDefined(descriptions[uri])) {
+                    if (angular.isDefined(descriptions[uri][typeName])) {
+                        return $q.when(descriptions[uri][typeName]);
+                    }
+                }
+
+                return requestFeatureTypeDescription(uri, typeName).then(function (data) {
+                    if (!angular.isDefined(descriptions[uri])) {
+                        descriptions[uri] = {};
+                    }
+                    descriptions[uri][typeName] = data;
+                    return descriptions[uri][typeName];
+                });
+            },
+            
+            // (GetFeature)
+            getFeature: function (url, typeName, paramOverrides) {
+                // TODO validate the url.
+                var uri = $filter('endpoint')(url, 'wfs');
+                return getFeature(uri, typeName, paramOverrides);
+            }
+        };
     }]);
+
--- src/main/ui/src/app/targetrank/targetRank.js
+++ src/main/ui/src/app/targetrank/targetRank.js
@@ -5,7 +5,8 @@ angular.module('stealth.targetrank.targetRank', [
     'stealth.common.panes.centerTop',
     'stealth.common.panes.centerRight',
     'stealth.targetrank.leftNav',
-    'stealth.ows.ows'
+    'stealth.ows.ows',
+    'ui.bootstrap.buttons'
 ])
 
     .config(['$routeProvider', function ($routeProvider) {
@@ -14,59 +15,128 @@ angular.module('stealth.targetrank.targetRank', [
         });
     }])
 
-    .controller('TargetRankController', ['$scope', '$rootScope', '$modal', '$http', 'WMS', function($scope, $rootScope, $modal, $http, WMS) {
+    .controller('TargetRankController', ['$scope', '$rootScope', '$modal', '$http', '$filter', 'WMS', 'WFS', 'CONFIG', function($scope, $rootScope, $modal, $http, $filter, WMS, WFS, CONFIG) {
 
         $scope.targetRank = {
             isLeftPaneVisible: true,
             leftPaneView: 'analysis',
             numSites: 0,
             numData: 0,
-            numTargets: 0,
-            sites: [{properties: {STATE_NAME: '(none)'}}]
+            numTargets: 0
         };
 
-        $scope.addSites = {
-            formData: {
-                service: 'WFS',
-                version: '2.0.0',
-                request: 'GetFeature',
-                typeName: 'topp:states',
-                outputFormat: 'application/json'
-            },
-            showWindow: function () {
-                $modal.open({
-                    scope: $scope,
-                    templateUrl: 'targetrank/addSitesForm.tpl.html',
-                    controller: function ($scope, $modalInstance) {
-                        
-                        // Get the layer list from the GetCapabilities WMS operation.
-                        WMS.getCapabilities().then(function (data) {
-                            var layers = data.capability.layers;
-                            $scope.addSites.layers = layers;
-                            $scope.addSites.currentLayer = $scope.addSites.currentLayer || layers[0];
-                        });
+        // Geoserver url
+        $scope.serverData = {
+            // The value the user enters into the form.
+            proposedGeoserverUrl: CONFIG.geoserver.url,
+            // The value after the users clicks 'Choose'.
+            currentGeoserverUrl: null
+        };
 
-                        $scope.addSites.submit = function () {
-                            var cql_filter = $scope.addSites.formData.cql_filter;
-                            $http.get('cors/http://localhost:8081/geoserver/wfs', {params: $scope.addSites.formData})
-                                .success(function (data) {
-                                    $modalInstance.close();
-                                    $rootScope.$broadcast("AddWmsMapLayer", {
-                                        name: 'Sites',
-                                        url: 'http://localhost:8081/geoserver/wms',
-                                        layers: [$scope.addSites.currentLayer.name],
-                                        cql_filter: cql_filter
-                                    });
-                                    $scope.$parent.targetRank.numSites = data.totalFeatures;
-                                    $scope.$parent.targetRank.sites = data.features;
-                                });
-                        };
+        // Layers returned from a GetCapabilities query.
+        $scope.layerData = {
+            layers: null,
+            currentLayer: null,
+            currentLayerDescription: null
+        };
 
-                        $scope.addSites.cancel = function () {
-                            $modalInstance.dismiss('cancel');
-                        };
+        $scope.inputData = {};
+        $scope.filterData = {};
+        $scope.addSites = {};
+        
+        // Used to display form fields in a step-by-step manner.
+        $scope.addSites.formStep = function () {
+            var step = 1; // Show the server url input
+            if ($scope.serverData.currentGeoserverUrl && !$scope.serverData.error) {
+                step = 2; // Show the input type select 
+                if ($scope.inputData.type) {
+                    step = 3; // Show the layer select input
+                    if($scope.layerData.currentLayer && !$scope.layerData.error) {
+                        step = 4; // Show the layer details and cql query input
                     }
-                });
+                }
+            } 
+            return step;
+        };
+        
+        // When the user changes the input data type, call getCapabilities
+        $scope.$watch('inputData.type', function (newVal, oldVal) {
+            if (newVal !== oldVal) {
+                $scope.addSites.updateServer();
             }
+        });
+
+        // Invoked when the user clicks the 'Choose' button on the server url field.
+        $scope.addSites.updateServer = function () {
+            $scope.serverData.currentGeoserverUrl = $scope.serverData.proposedGeoserverUrl;
+            $scope.layerData = {};
+            $scope.filterData = {};
+           
+            // Get the layer list from the GetCapabilities WMS operation.
+            WMS.getCapabilities($scope.serverData.currentGeoserverUrl).then(function (data) {
+                var layers = data.capability.layers;
+                $scope.serverData.error = null;
+                $scope.layerData.layers = layers;
+            }, function (reason) {
+                // The GetCapabilites request failed.
+                $scope.serverData.error = 'GetCapabilities request failed. Error: ' + reason.status + ' ' + reason.statusText;
+            });
+        };
+        
+        // Invoked when the current selected layer changes.
+        $scope.addSites.getFeatureTypeDescription = function () {
+            WFS.getFeatureTypeDescription($scope.serverData.currentGeoserverUrl, $scope.layerData.currentLayer.name).then(function (data) {
+                $scope.layerData.error = null;
+                $scope.featureTypeData = data;
+                $scope.filterData = {};
+
+                if (data.error) {
+                    $scope.featureTypeData = 'unavailable';
+                    // Response is successfull, but no description is found for the type.
+                }
+            }, function (reason) {
+                $scope.serverData.error = 'GetFeatureTypeDescription request failed. Error: ' + reason.status + ' ' + reason.statusText;
+            });
+        };
+
+        $scope.addSites.getFeature = function () {
+            var url = $scope.serverData.currentGeoserverUrl,
+                layerName = $scope.layerData.currentLayer.name,
+                cql = $scope.filterData.cql;
+
+            WFS.getFeature(url, layerName, {cql_filter: cql}).then(function (response) {
+                $scope.targetRank.numSites = response.data.totalFeatures;
+                $scope.targetRank.sites = response.data.features;
+                
+                // Update the map.
+                $rootScope.$broadcast("AddWmsMapLayer", {
+                    name: 'Sites',
+                    url: $filter('endpoint')(url, 'wms'),
+                    layers: [layerName],
+                    cql_filter: cql
+                });
+
+            }, function (response) {
+                // TODO - handle error. Clear the map on failure?
+            });
+        };
+        
+        $scope.addSites.submit = function () {
+            $scope.addSites.getFeature();
+        };
+
+        $scope.addSites.showWindow = function () {
+            $modal.open({
+                scope: $scope,
+                backdrop: 'static',
+                templateUrl: 'targetrank/addSitesForm.tpl.html',
+                controller: function ($scope, $modalInstance) {
+                    $scope.modal = {
+                        cancel: function () {
+                            $modalInstance.dismiss('cancel');
+                        }
+                    };
+                }
+            });
         };
     }]);
